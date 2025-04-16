76234d10638c3a192a1e75d5070bb30265cca390
todok0317 <todok0317@naver.com>
2025-04-16 11:12:14 +0900

    // Lv1-3 : 코드 개선 퀴즈 - Validation

--- src/main/java/org/example/expert/domain/user/dto/request/UserChangePasswordRequest.java
+++ src/main/java/org/example/expert/domain/user/dto/request/UserChangePasswordRequest.java
@@ -1,6 +1,8 @@
 package org.example.expert.domain.user.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,6 +14,11 @@ public class UserChangePasswordRequest {
 
     @NotBlank
     private String oldPassword;
-    @NotBlank
+
+    // Lv1-3 : 코드 개선 퀴즈 - Validation
+    @NotBlank(message = "새 비밀번호는 필수입니다.")
+    @Size(min = 8, message = "새 비밀번호는 최소 8자 이상이어야 합니다.")
+    @Pattern(regexp = ".*\\d.*", message = "새 비밀번호에는 최소 하나의 숫자가 포함되어야 합니다.")
+    @Pattern(regexp = ".*[A-Z].*", message = "새 비밀번호에는 최소 하나의 대문자가 포함되어야 합니다.")
     private String newPassword;
 }
